4fe0f39da10dd383b515b1e768cacc6c6d2f96cc
Sh4anT0r0n <xtruongtdo2003@outlook.com>
2022-11-11 21:26:43 +0700

    First commit

---
+++ Du_An_1_Nhom_4/src/main/java/Application/DuAn1.java
@@ -0,0 +1,14 @@
+
+
+package Application;
+
+/**
+ *
+ * @author Xuan Truong
+ */
+public class DuAn1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
