43e75c544fef5190c14c62be347d54f2c5a647d2
Sh4anT0r0n <xtruongtdo2003@outlook.com>
2022-11-12 11:17:11 +0700

    Test thay đổi

--- Du_An_1_Nhom_4/src/main/java/Application/DuAn1.java
+++ Du_An_1_Nhom_4/src/main/java/Application/DuAn1.java
@@ -9,6 +9,6 @@ package Application;
 public class DuAn1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Xin chao");
     }
 }
