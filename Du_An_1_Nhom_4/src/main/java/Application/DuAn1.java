fbb431504f8436f442632271968ee67ee4f2a855
Sh4anT0r0n <xtruongtdo2003@outlook.com>
2022-11-12 11:48:38 +0700

    Test lại trên folder mới

--- Du_An_1_Nhom_4/src/main/java/Application/DuAn1.java
+++ Du_An_1_Nhom_4/src/main/java/Application/DuAn1.java
@@ -9,6 +9,6 @@ package Application;
 public class DuAn1 {
 
     public static void main(String[] args) {
-        System.out.println("Xin chao");
+        System.out.println("Xin chao, t mới sửa đây. Test lại nhé");
     }
 }
