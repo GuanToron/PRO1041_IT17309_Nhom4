6a5a9667a3ca8bbb542477baa48f080f534c3d44
Xuan Truong <92640035+GuanToron@users.noreply.github.com>
2022-11-20 13:43:22 +0700

    Update DuAn1.java

--- Du_An_1_Nhom_4/src/main/java/Application/DuAn1.java
+++ Du_An_1_Nhom_4/src/main/java/Application/DuAn1.java
@@ -11,6 +11,6 @@ import View.MainApp;
 public class DuAn1 {
 
     public static void main(String[] args) {
-         new KhachHangServiceImplement().listKH().forEach(s->System.out.println(s.toString()));
+         
     }
 }
